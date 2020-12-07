14034d8021c0257fb288d7bb3e84452e9a82aee2
Grant <grantjames@live.co.uk>
2020-12-07 13:19:53 +0000

    Day 7

---
+++ 2020/7/part1.py
@@ -0,0 +1,30 @@
+import re
+
+f = open("input.txt", "r")
+input = f.readlines()
+f.close()
+
+# generate all the rules
+rules = {}
+for line in input:
+    key = re.match(r"^\w+ \w+", line).group(0)
+    value = re.findall(r"(\d) (\w+ \w+)", line)
+    rules[key] = value
+
+
+def can_hold(target_colour, parent_bag):
+    result = False
+    for rule in rules[parent_bag]:
+        if rule[1] == target_colour:
+            result = True
+        else:
+            result = result or can_hold(target_colour, rule[1])
+    
+    return result
+
+total = 0
+for bag in rules:
+    if can_hold("shiny gold", bag):
+        total += 1
+    
+print(total)
---
+++ 2020/7/part2.py
@@ -0,0 +1,21 @@
+import re
+
+f = open("input.txt", "r")
+input = f.readlines()
+f.close()
+
+# generate all the rules
+rules = {}
+for line in input:
+    key = re.match(r"^\w+ \w+", line).group(0)
+    value = re.findall(r"(\d) (\w+ \w+)", line)
+    rules[key] = value
+
+def bag_count(colour, depth):
+    total = 0
+    for rule in rules[colour]:
+        total += int(rule[0]) + (int(rule[0]) * bag_count(rule[1], depth+1))
+    
+    return total
+
+print(bag_count("shiny gold", 1))
